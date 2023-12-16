6ef132822a62d348ff3b5fb8693b63cf16787ed7
Paul Jang <pjang91@gmail.com>
2023-12-16 16:25:37 -0500

    matcher auth - protected routes

---
+++ src/app/[id]-acquisition/page.tsx
@@ -0,0 +1,3 @@
+export default function Acquisition() {
+  return <div>Acquisition</div>;
+}
---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
+
+export default withMiddlewareAuthRequired();
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - about (about page)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - $ (homepage)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|$).*)",
+  ],
+};
