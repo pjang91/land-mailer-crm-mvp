3503a2ef09ff4206a79c96e326408454241e00ce
Paul Jang <pjang91@gmail.com>
2023-12-16 15:00:49 -0500

    authentication works - need to protect route

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,15 @@
+import { getSession } from "@auth0/nextjs-auth0";
+
 export default async function DashboardPage() {
-  return <div>This page needs to be protected!</div>;
+  const session = await getSession();
+  return (
+    <div>
+      {!!session?.user && (
+        <div>
+          {session.user.email} - <a href="/api/auth/logout">Logout</a>
+        </div>
+      )}
+      This page needs to be protected!
+    </div>
+  );
 }
